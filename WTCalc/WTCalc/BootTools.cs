7b9e425e00f63b7cabbe25fffc4c0c11d46aad77
stackprobe <stackprobes@gmail.com>
2019-01-26 00:47:01 +0900

    coffee break

--- WTCalc/WTCalc/BootTools.cs
+++ WTCalc/WTCalc/BootTools.cs
@@ -6,6 +6,8 @@ using System.IO;
 
 namespace WTCalc
 {
+	// sync > @ BootTools
+
 	public class BootTools
 	{
 		public static string SelfFile;
@@ -19,4 +21,6 @@ namespace WTCalc
 			Directory.SetCurrentDirectory(SelfDir);
 		}
 	}
+
+	// < sync
 }
