86843e50af793d020027a8ac0af5f6ef991e5c5f
stackprobe <stackprobes@gmail.com>
2019-01-27 01:28:15 +0900

    daily

--- WTCalc/WTCalc/Program.cs
+++ WTCalc/WTCalc/Program.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Windows.Forms;
-using System.Threading;
 using System.IO;
 using System.Text;
-using Charlotte;
+using System.Threading;
+using System.Windows.Forms;
 
 namespace WTCalc
 {
